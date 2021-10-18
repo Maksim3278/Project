6a70651c2bcac649ecbdea20e1e75f0fe1a65f49
Unicode <51852469+Maksim3278@users.noreply.github.com>
2021-10-18 14:31:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
+import telebot
+from telebot import types
 
+TOKEN = '2094289863:AAG4p7PK5lbRcwL-LXKvwAcXkWU4eeeC7FA'
+bot = telebot.TeleBot(TOKEN)
+
+#Функция приветствия бота с пользователем:
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEDG4hhbVdLXiXszV5C8BO_OujZE-EGfQAC6AQAAvPyjj-be41RjBUoYyEE') #Отправка стикера
+
+    #плашка с клавишами
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item_help = types.KeyboardButton('Помощь')
+    item_start = types.KeyboardButton('Начать')
+
+    markup.add(item_help, item_start)
